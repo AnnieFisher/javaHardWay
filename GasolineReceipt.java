af98cc3e241fa29b7680cdff6cad13b47c575a01
AnnieFisher <annielynn.fisher@gmail.com>
2016-07-16 11:16:06 -0600

    add exercise 2

---
+++ GasolineReceipt.java
@@ -0,0 +1,18 @@
+public class GasolineReceipt
+{
+  public static void main(String[] args)
+  {
+    System.out.println("+------------------------+") ;
+    System.out.println("|                        |");
+    System.out.println("|     CORNER STORE       |");
+    System.out.println("|                        |");
+    System.out.println("| 2016-07-13 11:03AM     |");
+    System.out.println("|                        |");
+    System.out.println("| Gallons:       15.210  |");
+    System.out.println("| Price/gallon: $ 2.230  |");
+    System.out.println("|                        |");
+    System.out.println("| Fuel total:  $ 33.92   |");
+    System.out.println("|                        |");
+    System.out.println("+------------------------+");
+  }
+}
