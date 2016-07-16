09c87ff73fc9ce7e808cd8a0ce1c48d0ee0090ae
AnnieFisher <annielynn.fisher@gmail.com>
2016-07-16 10:57:50 -0600

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,8 @@
+public class FirstProg
+{
+  public static void main( String[] args )
+  {
+    System.out.println( "I, Annie Fisher, am determined to learn how to code." );
+    System.out.println( "Today's date is July 13, 2016." );
+  }
+}
